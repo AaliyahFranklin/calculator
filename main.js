7a88c176a309b58eeea3faa75049d9368c05d3a9
AaliyahFranklin <aaliyahf.aqf@gmail.com>
2026-04-23 13:16:09 -0700

    fixed window size

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const { app, BrowserWindow, Menu } = require('electron')
 
 app.whenReady().then(() => {
   Menu.setApplicationMenu(null)  // removes menu bar
-  const win = new BrowserWindow({ width: 500, height: 800 })
+  const win = new BrowserWindow({ width: 500, height: 710 })
   win.loadFile('index.html')
 })
